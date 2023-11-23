f7c5947c3bb0f93e31d9bacabedc6481ee7202fd
propiony <46004787+propiony@users.noreply.github.com>
2023-11-23 19:42:06 +0530

    added responsive behaviour

--- yes-germany/src/components/Header.jsx
+++ yes-germany/src/components/Header.jsx
@@ -1,14 +1,23 @@
 import ButtonPrimary from "./common/ButtonPrimary";
-import Dubai from "../assets/images/dubai.png";
+import Dubai from "../assets/images/ger.png";
 import India from "../assets/images/india.png";
 const Header = () => (
   <div className="d-flex py-2 header-sec home-container ">
-    {/* <div>
+    <div className="country-contacts d-flex flex-row">    <div className="header-contact d-flex">
       <img src={India} alt="india contact" />
+      <div className="contact-details d-flex flex-column ms-2">
+        <div className="country">India</div>
+        <a className="contact-link" href="tel:+91-8070606070">+91-8070606070</a>
+      </div>
     </div>
-    <div>
-      <img src={Dubai} alt="dubai-contact" />
-    </div> */}
+    <div className="header-contact d-flex ms-2">
+      <img src={Dubai} alt="india contact" />
+      <div className="contact-details d-flex flex-column ms-2">
+        <div className="country">Dubai</div>
+        <a className="contact-link" href="tel:+971506071022">+971506071022</a>
+      </div>
+    </div></div>
+
     <div className="ms-auto align-self-center">
       <ButtonPrimary text="Arrange a Callback" />
     </div>
--- yes-germany/src/components/InfoSection.js
+++ yes-germany/src/components/InfoSection.js
@@ -3,7 +3,7 @@ const InfoSection = () => {
   return (
     <div className="info-section">
       <div className="home-container">
-        <h1>Study in Germany</h1>
+        <h1 className="info-header">Study in Germany</h1>
         <p className="para-1">Discover the Land of Educational Excellence</p>
         <p className="para-2">
           Germany is the best option where they magnify the candidates area of
@@ -17,19 +17,19 @@ const InfoSection = () => {
           candidates.
         </p>
         <div className="statistics row">
-          <div className="col-3">
+          <div className="col-12 col-sm-6 col-lg-3">
             <h1>17,000+</h1>
             <p>Programs Available to Choose</p>
           </div>
-          <div className="col-3">
+          <div className="col-12 col-sm-6 col-lg-3">
             <h1>5.25 M +</h1>
             <p>Return on investment</p>
           </div>
-          <div className="col-3">
+          <div className="col-12 col-sm-6 col-lg-3">
             <h1>17 Lakh +</h1>
             <p>German Vacant Jobs</p>
           </div>
-          <div className="col-3">
+          <div className="col-12 col-sm-6 col-lg-3">
             <h1>18 Month</h1>
             <p>Return on investment</p>
           </div>
